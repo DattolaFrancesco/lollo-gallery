16a13a73311dbbb8543bba95347bf5482555372a
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-03-02 13:04:52 +0100

    .

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <main className="flex justify-center items-center flex-col">
+      <main className="flex justify-center items-center flex-col ">
         {Array.from({ length: 10 }).map((e, i) => {
           return (
-            <div key={i} className="w-1/2 h-svh flex justify-center items-center">
+            <div key={i} className="w-screen min-h-screen flex justify-center items-center">
               <Image src="/bigbang.jpg" alt="foto" width={800} height={1200} />
             </div>
           );
