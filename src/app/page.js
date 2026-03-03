c177262e4c2c05b7b430a79d990b07c4ece8ecc8
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-03-03 12:09:08 +0100

    added timeline for all the imgs

--- src/app/page.js
+++ src/app/page.js
@@ -11,64 +11,34 @@ export default function Home() {
   const imagesRef = useRef([]);
   const containerRef = useRef(null);
   useGSAP(() => {
-    const dx = 50;
-    const sx = -50;
-    imagesRef.current.forEach((img, i) => {
-      let position;
-      if (i % 2 === 0) position = dx;
-      else position = sx;
-      const photo = img.querySelector(".photo");
-      const tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: img,
-          start: "top 100%",
-          end: "bottom 0%",
-          scrub: 0.5,
-        },
-      });
-      tl.from(img, {
-        xPercent: position,
-        opacity: 0.1,
-        scale: 0.2,
-      });
-      tl.to(img, {
-        xPercent: 0,
-        opacity: 1,
-        scale: 1.2,
-      });
-      tl.to(img, {
-        xPercent: -position,
-        opacity: 0.1,
-        scale: 0.2,
-      });
+    // timeline-------
+    const totalScroll = imagesRef.current.length * window.innerHeight * 2;
+    const tl = gsap.timeline();
 
-      // gsap.set(img, {
-      //   xPercent: 100,
-      //   opacity: 0.6,
-      //   scale: 0.2,
-      // });
-      // gsap.to(img, {
-      //   xPercent: -150,
-      //   opacity: 1,
-      //   scale: 1.2,
-      //   scrollTrigger: {
-      //     trigger: photo,
-      //     start: "top 150%",
-      //     end: "bottom bottom", // piccolo tratto di scroll per completare
-      //     scrub: 4, // anima lentamente legata allo scroll
-      //   },
-      // });
+    imagesRef.current.forEach((img, i) => {
+      const direction = i % 2 === 0 ? 1 : -1;
+      tl.fromTo(
+        img,
+        { xPercent: 100 * direction, yPercent: -100 * direction, autoAlpha: 0, scale: 0.2 },
+        { xPercent: 0, yPercent: 0, autoAlpha: 1, scale: 1.5, duration: 1.5 },
+        i * 1.5,
+      ).to(img, { xPercent: -100 * direction, yPercent: -100 * direction, autoAlpha: 0, scale: 0.2, duration: 1.5 }, i * 1.5 + 1.5);
+    });
+
+    ScrollTrigger.create({
+      trigger: containerRef.current,
+      start: "top top",
+      end: "+=" + totalScroll,
+      scrub: 1,
+      pin: true,
+      animation: tl,
     });
   }, []);
   return (
-    <div ref={containerRef} className="bg-lime-900 min-h-[100svh] flex justify-center items-center flex-col overflow-hidden">
+    <div ref={containerRef} className="relative h-screen overflow-hidden bg-lime-900 flex items-center justify-center">
       {Array.from({ length: 10 }).map((e, i) => {
         return (
-          <div
-            ref={(el) => (imagesRef.current[i] = el)}
-            style={{ willChange: "transform, opacity, scale" }}
-            className=" w-1/2 min-h-[100svh] flex items-center "
-          >
+          <div ref={(el) => (imagesRef.current[i] = el)} style={{ willChange: "transform, opacity, scale" }} className=" absolute w-1/2 flex justify-center ">
             <Image src="/bigbang.jpg" alt="foto" width={800} height={1200} className="photo" />
           </div>
         );
