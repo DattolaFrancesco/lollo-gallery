e3867162ee876c04e17e8afbdf33f879c31e585b
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-03-05 15:10:28 +0100

    .

--- src/app/components/DesktopGallery.jsx
+++ src/app/components/DesktopGallery.jsx
@@ -4,7 +4,7 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import Draggable from "gsap/dist/Draggable";
 import InertiaPlugin from "gsap/dist/InertiaPlugin";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 gsap.registerPlugin(Draggable, InertiaPlugin);
 
@@ -46,6 +46,27 @@ const gallery = [
 export default function DesktopGallery() {
   const containerRef = useRef(null);
   const imgsRef = useRef([]);
+  const [loaded, setLoaded] = useState(false);
+  const loadImage = (src) => {
+    return new Promise((resolve, reject) => {
+      const img = new window.Image();
+      img.src = src;
+      img.onload = () => resolve(src);
+      img.onerror = () => reject(src);
+    });
+  };
+  const preloadImages = async (images) => {
+    const promises = images.map((src) => loadImage(src));
+    await Promise.all(promises);
+  };
+  useEffect(() => {
+    const init = async () => {
+      await preloadImages(gallery);
+      setLoaded(true);
+    };
+
+    init();
+  }, []);
   const randomNumber = () => {
     let n = Math.floor(Math.random() * 90);
     while (n < 7) {
@@ -62,18 +83,17 @@ export default function DesktopGallery() {
 
   useGSAP(
     () => {
+      if (!loaded) return;
       const img = document.querySelectorAll(".imgs");
-      gsap.set(img, {
-        xPercent: 0,
-        yPercent: 0,
-      });
+      gsap.set(img, { opacity: 0, x: 0, y: 0 });
 
       gsap.to(img, {
-        duration: 1,
+        duration: 1.2,
+        opacity: 1,
         x: () => `${randomNumber()}vw`,
         y: () => `${randomNumber()}vh`,
-        stagger: 0.09,
-        ease: "power1.inOut",
+        stagger: 0.1,
+        ease: "power1.out",
       });
 
       Draggable.create(img, {
@@ -91,7 +111,7 @@ export default function DesktopGallery() {
         },
       });
     },
-    { scope: containerRef },
+    { scope: containerRef, dependencies: [loaded] },
   );
   return (
     <div
@@ -101,22 +121,24 @@ export default function DesktopGallery() {
         clearScale();
       }}
     >
-      {gallery.map((e, i) => {
-        return (
-          <div
-            key={i}
-            className=" imgs absolute w-1/12 "
-            onClick={(e) => {
-              e.stopPropagation();
-              gsap.to(e.currentTarget, {
-                scale: 5,
-              });
-            }}
-          >
-            <Image src={e} alt="foto" width={800} height={1200} />
-          </div>
-        );
-      })}
+      {!loaded && <h1 className="text-red-700">ciao sto caricando...</h1>}
+      {loaded &&
+        gallery.map((e, i) => {
+          return (
+            <div
+              key={i}
+              className=" imgs absolute w-1/12 "
+              onClick={(e) => {
+                e.stopPropagation();
+                gsap.to(e.currentTarget, {
+                  scale: 2,
+                });
+              }}
+            >
+              <Image src={e} alt="foto" width={800} height={1200} />
+            </div>
+          );
+        })}
     </div>
   );
 }
