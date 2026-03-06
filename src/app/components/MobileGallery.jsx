d4921e02e5b523756a0b082c98406f605dd244b0
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-03-06 11:53:12 +0100

    .

--- src/app/components/MobileGallery.jsx
+++ src/app/components/MobileGallery.jsx
@@ -1,101 +1,364 @@
+// "use client";
+// import Image from "next/image";
+// import gsap from "gsap";
+// import { useGSAP } from "@gsap/react";
+// import ScrollTrigger from "gsap/src/ScrollTrigger";
+// import { useRef } from "react";
+
+// gsap.registerPlugin(ScrollTrigger);
+
+// export default function MobileGallery() {
+//   const gallery = [
+//     "/gallery/1.jpg",
+//     "/gallery/2.jpg",
+//     "/gallery/3.jpg",
+//     "/gallery/4.jpg",
+//     "/gallery/5.jpg",
+//     "/gallery/6.jpg",
+//     "/gallery/7.jpg",
+//     "/gallery/8.jpg",
+//     "/gallery/9.jpg",
+//     "/gallery/10.jpg",
+//     "/gallery/11.jpg",
+//     "/gallery/12.jpg",
+//     "/gallery/13.jpg",
+//     "/gallery/14.jpg",
+//     "/gallery/15.jpg",
+//     "/gallery/16.jpg",
+//     "/gallery/17.jpg",
+//     "/gallery/18.jpg",
+//     "/gallery/19.jpg",
+//     "/gallery/20.jpg",
+//     "/gallery/21.jpg",
+//     "/gallery/22.jpg",
+//     "/gallery/23.jpg",
+//     "/gallery/24.jpg",
+//     "/gallery/25.jpg",
+//     "/gallery/26.jpg",
+//     "/gallery/27.jpg",
+//     "/gallery/28.jpg",
+//     "/gallery/29.jpg",
+//     "/gallery/30.jpg",
+//     "/gallery/31.jpg",
+//     "/gallery/32.jpg",
+//   ];
+//   const imagesRef = useRef([]);
+//   const containerRef = useRef(null);
+//   const random = () => {
+//     return Math.floor(Math.random() * 120);
+//   };
+//   const randomDir = () => {
+//     const n = Math.random() * 1;
+//     if (n > 0.5) return -1;
+//     else return 1;
+//   };
+//   useGSAP(() => {
+//     const tl = gsap.timeline({
+//       scrollTrigger: {
+//         trigger: containerRef.current,
+//         start: "top top",
+//         end: `+=${gallery.length * 100 * 2}%`,
+//         scrub: true,
+//         pin: true,
+//         markers: true,
+//         snap: {
+//           snapTo: "labels",
+//           duration: 0.5,
+//         },
+//       },
+//     });
+//     imagesRef.current.forEach((img, i) => {
+//       gsap.set(img, { left: `${randomDir() * random()}%`, top: `${randomDir() * random()}%`, opacity: 0 });
+
+//       tl.to(img, {
+//         left: "25%",
+//         top: "0%",
+//         opacity: 1,
+//         scale: 1.8,
+//       });
+//       tl.addLabel(`img-${i}`);
+//     });
+//   }, []);
+
+//   return (
+//     <>
+//       <div ref={containerRef} className=" overflow-hidden  min-h-[100svh] flex justify-center items-center flex-col relative">
+//         {gallery.map((e, i) => {
+//           return (
+//             <div
+//               key={i}
+//               ref={(el) => (imagesRef.current[i] = el)}
+//               style={{ willChange: "transform, opacity, scale" }}
+//               className=" w-1/2 h-screen flex items-center absolute test origin-center"
+//             >
+//               <Image src={e} alt="foto" width={800} height={1200} className="photo" />
+//             </div>
+//           );
+//         })}
+//       </div>
+//     </>
+//   );
+// }
 "use client";
 import Image from "next/image";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-import ScrollTrigger from "gsap/src/ScrollTrigger";
-import { useRef } from "react";
+import Draggable from "gsap/dist/Draggable";
+import InertiaPlugin from "gsap/dist/InertiaPlugin";
+import { useEffect, useRef, useState } from "react";
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(Draggable, InertiaPlugin);
+
+const gallery = [
+  "/gallery/1.jpg",
+  "/gallery/2v.jpg",
+  "/gallery/3.jpg",
+  "/gallery/4.jpg",
+  "/gallery/5.jpg",
+  "/gallery/6.jpg",
+  "/gallery/7v.jpg",
+  "/gallery/8.jpg",
+  "/gallery/9.jpg",
+  "/gallery/10v.jpg",
+  "/gallery/11.jpg",
+  "/gallery/12.jpg",
+  "/gallery/13.jpg",
+  "/gallery/14.jpg",
+  "/gallery/15.jpg",
+  "/gallery/16.jpg",
+  "/gallery/17.jpg",
+  "/gallery/18.jpg",
+  "/gallery/19.jpg",
+  "/gallery/20.jpg",
+  "/gallery/21.jpg",
+  "/gallery/22.jpg",
+  "/gallery/23.jpg",
+  "/gallery/24v.jpg",
+  "/gallery/25.jpg",
+  "/gallery/26.jpg",
+  "/gallery/27v.jpg",
+  "/gallery/28v.jpg",
+  "/gallery/29.jpg",
+  "/gallery/30.jpg",
+  "/gallery/31.jpg",
+  "/gallery/32.jpg",
+  "/gallery/33v.webp",
+  "/gallery/34v.webp",
+  "/gallery/35v.webp",
+  "/gallery/36.webp",
+  "/gallery/37.webp",
+  "/gallery/38.webp",
+  "/gallery/39.webp",
+  "/gallery/40.webp",
+  "/gallery/41.webp",
+  "/gallery/42.webp",
+  "/gallery/43.webp",
+  "/gallery/44.webp",
+  "/gallery/45.webp",
+  "/gallery/46.webp",
+  "/gallery/47.webp",
+  "/gallery/48.webp",
+  "/gallery/49.webp",
+  "/gallery/50.jpg",
+  "/gallery/51.jpg",
+  "/gallery/52.jpg",
+  "/gallery/53.jpg",
+  "/gallery/54.jpg",
+];
 
 export default function MobileGallery() {
-  const gallery = [
-    "/gallery/1.jpg",
-    "/gallery/2.jpg",
-    "/gallery/3.jpg",
-    "/gallery/4.jpg",
-    "/gallery/5.jpg",
-    "/gallery/6.jpg",
-    "/gallery/7.jpg",
-    "/gallery/8.jpg",
-    "/gallery/9.jpg",
-    "/gallery/10.jpg",
-    "/gallery/11.jpg",
-    "/gallery/12.jpg",
-    "/gallery/13.jpg",
-    "/gallery/14.jpg",
-    "/gallery/15.jpg",
-    "/gallery/16.jpg",
-    "/gallery/17.jpg",
-    "/gallery/18.jpg",
-    "/gallery/19.jpg",
-    "/gallery/20.jpg",
-    "/gallery/21.jpg",
-    "/gallery/22.jpg",
-    "/gallery/23.jpg",
-    "/gallery/24.jpg",
-    "/gallery/25.jpg",
-    "/gallery/26.jpg",
-    "/gallery/27.jpg",
-    "/gallery/28.jpg",
-    "/gallery/29.jpg",
-    "/gallery/30.jpg",
-    "/gallery/31.jpg",
-    "/gallery/32.jpg",
-  ];
-  const imagesRef = useRef([]);
   const containerRef = useRef(null);
-  const random = () => {
-    return Math.floor(Math.random() * 120);
-  };
-  const randomDir = () => {
-    const n = Math.random() * 1;
-    if (n > 0.5) return -1;
-    else return 1;
-  };
-  useGSAP(() => {
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: containerRef.current,
-        start: "top top",
-        end: `+=${gallery.length * 100 * 2}%`,
-        scrub: true,
-        pin: true,
-        markers: true,
-        snap: {
-          snapTo: "labels",
-          duration: 0.5,
-        },
-      },
+  const btnShuffleRef = useRef(null);
+  const [btnGrid, setBtnGrid] = useState("Grid");
+  const [loaded, setLoaded] = useState(false);
+  const [lastClicked, setLastClicked] = useState(null);
+  const [lastClickedDescription, setLastClickedDescription] = useState(null);
+  const loadImage = (src) => {
+    return new Promise((resolve, reject) => {
+      const img = new window.Image();
+      img.src = src;
+      img.onload = () => {
+        resolve(src);
+        console.log("img caricata");
+      };
+      img.onerror = () => reject(src);
     });
-    imagesRef.current.forEach((img, i) => {
-      gsap.set(img, { left: `${randomDir() * random()}%`, top: `${randomDir() * random()}%`, opacity: 0 });
+  };
+  const preloadImages = async (images) => {
+    const promises = images.map((src) => loadImage(src));
+    await Promise.all(promises);
+  };
+  useEffect(() => {
+    const init = async () => {
+      await preloadImages(gallery);
+      setLoaded(true);
+    };
 
-      tl.to(img, {
-        left: "25%",
-        top: "0%",
-        opacity: 1,
-        scale: 1.8,
-      });
-      tl.addLabel(`img-${i}`);
-    });
+    init();
   }, []);
+  const randomNumber = () => {
+    let n = Math.floor(Math.random() * 37);
+    let direction = Math.random();
+    while (n < 7) {
+      n = Math.floor(Math.random() * 37);
+    }
+    if (direction > 0.5) return n;
+    else return -n;
+  };
+  const shuffle = () => {
+    const images = document.querySelectorAll(".imgs");
+    images.forEach((e) => {
+      const description = e.querySelector("div");
+      if (description) description.classList.add("hidden");
+      gsap.to(e, {
+        scale: 1,
+        x: () => `${randomNumber()}vw`,
+        y: () => `${randomNumber()}vh`,
+      });
+    });
+    setLastClicked(null);
+    setLastClickedDescription(null);
+  };
+  const Grid = () => {
+    const images = document.querySelectorAll(".imgs");
+    const container = containerRef.current;
+    const btnShuffle = btnShuffleRef.current;
+    const Body = document.querySelector("body");
+    setBtnGrid(!btnGrid);
+    Body.classList.toggle("overflow-x-hidden");
+    btnShuffle.classList.toggle("hidden");
+    container.classList.toggle("shuffle");
+    container.classList.toggle("columns-2");
+    container.classList.toggle("gap-0");
+    setLastClicked(null);
+    setLastClickedDescription(null);
+    images.forEach((e) => {
+      gsap.to(e, { scale: 1 });
+      e.classList.toggle("gridCustom");
+      e.classList.add("transitionCustom");
+      const description = e.querySelector("div");
+      if (description) description.classList.add("hidden");
+    });
+    setTimeout(() => {
+      images.forEach((e) => {
+        e.classList.remove("transitionCustom");
+      });
+    }, 1000);
+  };
+  const clearScale = () => {
+    const images = document.querySelectorAll(".imgs");
+    images.forEach((e) => {
+      gsap.to(e, { scale: 1 });
+    });
+    gsap.to(lastClicked, {
+      x: () => `${randomNumber()}vw`,
+      y: () => `${randomNumber()}vh`,
+    });
+    lastClickedDescription.classList.add("hidden");
+    setLastClicked(null);
+    setLastClickedDescription(null);
+  };
+  useGSAP(
+    () => {
+      if (!loaded) return;
+      const img = document.querySelectorAll(".imgs");
+      gsap.set(img, { x: 0, y: 0, opacity: 1 });
 
+      gsap.to(img, {
+        duration: 1.2,
+        x: () => `${randomNumber()}vw`,
+        y: () => `${randomNumber()}vh`,
+        stagger: 0.09,
+        ease: "power1.out",
+      });
+
+      Draggable.create(img, {
+        type: "x,y",
+        bounds: containerRef.current,
+        inertia: true,
+        edgeResistance: 0.8,
+        onClick() {
+          img.forEach((el) => {
+            gsap.to(el, {
+              scale: 1,
+            });
+          });
+        },
+      });
+    },
+    {
+      scope: containerRef,
+      dependencies: [loaded],
+    },
+  );
   return (
-    <>
-      <div ref={containerRef} className=" overflow-hidden  min-h-[100svh] flex justify-center items-center flex-col relative">
-        {gallery.map((e, i) => {
-          return (
-            <div
-              key={i}
-              ref={(el) => (imagesRef.current[i] = el)}
-              style={{ willChange: "transform, opacity, scale" }}
-              className=" w-1/2 h-screen flex items-center absolute test origin-center"
-            >
-              <Image src={e} alt="foto" width={800} height={1200} className="photo" />
+    <div
+      ref={containerRef}
+      className="w-screen min-h-screen relative  shuffle "
+      onClick={() => {
+        clearScale();
+      }}
+    >
+      <button
+        ref={btnShuffleRef}
+        onClick={(e) => {
+          e.stopPropagation();
+          shuffle();
+        }}
+        className="text-white  cursor-pointer fixed bottom-[10vh] z-99999999"
+      >
+        Lollo Gallery
+      </button>
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          Grid();
+        }}
+        className="text-white  cursor-pointer fixed top-[10vh] mx-[49%] z-99999999"
+      >
+        {btnGrid ? "Grid" : "Shuffle"}
+      </button>
+      {!loaded && <h1 className="text-red-700">ciao sto caricando...</h1>}
+      {gallery.map((e, i) => {
+        return (
+          <div
+            key={i}
+            className=" imgs absolute w-[30vw] opacity-0"
+            onClick={(e) => {
+              setLastClicked(e.currentTarget);
+              e.stopPropagation();
+              const vertical = e.currentTarget.querySelector("img").dataset.name;
+              if (vertical) {
+                gsap.to(e.currentTarget, {
+                  scale: 2,
+                  x: 0,
+                  y: 0,
+                });
+              } else {
+                gsap.to(e.currentTarget, {
+                  scale: 3,
+                  x: 0,
+                  y: 0,
+                });
+              }
+
+              gsap.to(lastClicked, {
+                x: () => `${randomNumber()}vw`,
+                y: () => `${randomNumber()}vh`,
+              });
+              const description = e.currentTarget.querySelector("div");
+              setLastClickedDescription(description);
+              description.classList.remove("hidden");
+              lastClickedDescription.classList.add("hidden");
+            }}
+          >
+            <Image src={e} alt="foto" width={800} height={1200} quality={100} className="relative" data-name={e.includes("v") ? "vertical" : undefined} />
+            <div className=" hidden ">
+              <p className="text-yellow-400 text-[1vw] bg-blue-700 ">descrixione cmdcsmkskmmkmcsmklcl;dl;dlm; bla bla bla</p>
             </div>
-          );
-        })}
-      </div>
-    </>
+          </div>
+        );
+      })}
+    </div>
   );
 }
