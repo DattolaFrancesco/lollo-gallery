8f8854b0324c640fa1a4705f5cf0627fa71d1c3d
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-03-15 12:09:14 +0100

    added hover gray to the btn

--- src/app/components/DesktopGallery.jsx
+++ src/app/components/DesktopGallery.jsx
@@ -377,7 +377,7 @@ export default function DesktopGallery() {
         className=" flex gap-0.5  absolute left-[50%]  top-[90%] -translate-1/2 m-2 w-[600] min-h-[60] z-999999999999 fixed top-0 generic1sTransition"
       >
         {/* logo */}
-        <div className=" w-[15%] bg-white flex justify-center items-center">
+        <div className=" w-[15%] bg-white hover:bg-gray-200 flex justify-center items-center">
           <button
             onClick={(e) => {
               e.stopPropagation();
@@ -399,7 +399,7 @@ export default function DesktopGallery() {
                 onClick={() => {
                   Grid();
                 }}
-                className="text-xs text-white bg-gray-200 p-1 activeBtn cursor-pointer"
+                className="text-xs text-white hover:bg-gray-300  bg-gray-200 p-1 activeBtn cursor-pointer"
               >
                 Shuffle
               </button>
@@ -409,18 +409,18 @@ export default function DesktopGallery() {
                   e.stopPropagation();
                   Grid();
                 }}
-                className="text-xs text-white bg-gray-200 p-1 cursor-pointer"
+                className="text-xs text-white hover:bg-gray-300 bg-gray-200 p-1 cursor-pointer"
               >
                 Grid
               </button>
             </div>
           </div>
-          <div className="bg-white">
+          <div className="bg-white hover:bg-gray-200">
             <a href="https://www.instagram.com/lollochef_/">
               <p className="text-xs px-1">Archive by Lorenzo Accorti</p>
             </a>
           </div>
-          <div className="bg-white">
+          <div className="bg-white hover:bg-gray-200">
             <a href="https://www.instagram.com/francescodattola_/">
               <p className="text-xs px-1">Developed by Francesco Dattola</p>
             </a>
@@ -428,7 +428,7 @@ export default function DesktopGallery() {
         </div>
         {/* fine btn */}
         {/* inizio shuffle */}
-        <div className="bg-white w-[5%] flex items-center justify-center flex-col">
+        <div className="bg-white hover:bg-gray-200 w-[5%] flex items-center justify-center flex-col">
           <button
             onClick={(e) => {
               e.stopPropagation();
